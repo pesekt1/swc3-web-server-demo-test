581229ba729b9b85911d8e9cd3be949cdfbbcde2
pesekt1 <58634005+pesekt1@users.noreply.github.com>
2020-10-17 14:47:32 +0200

    -

--- src/test/java/swc3/server2/Server2ApplicationTests.java
+++ src/test/java/swc3/server2/Server2ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Server2ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
