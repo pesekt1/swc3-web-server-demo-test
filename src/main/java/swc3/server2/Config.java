9d79ee7655ebfabe5d0fe85dc75ade9044ce62a8
pesekt1 <58634005+pesekt1@users.noreply.github.com>
2020-10-17 16:07:52 +0200

    -

---
+++ src/main/java/swc3/server2/Config.java
@@ -0,0 +1,36 @@
+package swc3.server2;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableAutoConfiguration(exclude = {
+        JpaRepositoriesAutoConfiguration.class
+})
+public class Config {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
+    @Value("${spring.datasource.username}")
+    private String dbUser;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        config.setPassword(dbPassword);
+        config.setUsername(dbUser);
+        return new HikariDataSource(config);
+    }
+}
